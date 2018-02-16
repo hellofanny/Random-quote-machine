fa3211526915b6ebabbf48d0d8b17847fb8240ba
fanny <fc2@cin.ufpe.br>
2018-02-16 20:43:14 -0300

    Choosing a background color randomly

--- script.js
+++ script.js
@@ -25,12 +25,24 @@ function sayHello(){
         default:
             greetingText = "Hello!";
     }
-    
+
     console.log("hee");
     
     document.getElementById("greeting").innerHTML = greetingText;
 }
 
+function pickRandomColor(){
+    var colors = ["#a0ab44", "#f8bfb9", "#f8bd76", "#b26c3e", "#f3a683",
+                "#b1bb39", "#778beb", "#f78fb3", "#3dc1d3", "#786fa6", "#303952",
+                "#ea8685", "#f5cd79", "#d98047", "#6db096"];
+
+    var colorIndex = Math.floor(Math.random() * colors.length);
+
+    //console.log(colors[colorIndex]);
+    document.body.style.backgroundColor = colors[colorIndex];
+}
+
 
 window.addEventListener("load", sayHello);
+window.addEventListener("load", pickRandomColor);
 
